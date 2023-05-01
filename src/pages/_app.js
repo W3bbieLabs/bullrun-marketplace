62f949bc04fb15002a58541f76411fa81bb16ccf
barigyasi <barigyasi@gmail.com>
2023-04-30 21:23:40 -0500

    Connect Wallet Added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const activeChain = "theta";
+
+function App({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider activeChain={activeChain}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
+
+export default App;
