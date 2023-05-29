3d504ac863ced8503c025c90b8c32e26bce48b07
ss251 <ss251@uw.edu>
2023-05-29 14:50:27 -0500

    display fullscreen video on large screens

--- src/components/video.js
+++ src/components/video.js
@@ -1,112 +1,86 @@
-import { useAddress, useDisconnect, useContract, useNetworkMismatch } from '@thirdweb-dev/react';
-
+import {
+  useAddress,
+  useDisconnect,
+  useContract,
+  useNetworkMismatch,
+} from "@thirdweb-dev/react";
 
 import { useState, useEffect } from "react";
 
-
-
-
-
-
 function Video() {
+  const address = useAddress();
 
-   const address = useAddress();
-   
-   const isMismatched = useNetworkMismatch();
-   const disconnectWallet = useDisconnect();
-   const edition = useContract("0x59336Fd357f07a6501B2444556ae98633B741297", "edition").contract
-   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
-   
-   useEffect(() => {
-     if (!address) {
-         return;
-     }
-   
-     const checkBalance = async () => {
-            try {
-             const nfts = await edition.getOwned(address);
-             setHasClaimedNFT(nfts?.length > 0);
-            } catch (error) {
-             setHasClaimedNFT(false);
-             
-            }
-          };
-          checkBalance();
-   }, [address , edition]);
-     
-  
-  
-  
-   //////**SIGN IN AREA */
-  
-   if (!address) {
-         return (
-             <>
-             <div className='font-link'>
-             <div className='eggz'>
-           
+  const isMismatched = useNetworkMismatch();
+  const disconnectWallet = useDisconnect();
+  const edition = useContract(
+    "0x59336Fd357f07a6501B2444556ae98633B741297",
+    "edition"
+  ).contract;
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+
+    const checkBalance = async () => {
+      try {
+        const nfts = await edition.getOwned(address);
+        setHasClaimedNFT(nfts?.length > 0);
+      } catch (error) {
+        setHasClaimedNFT(false);
+      }
+    };
+    checkBalance();
+  }, [address, edition]);
+
+  //////**SIGN IN AREA */
+
+  if (!address) {
+    return (
+      <>
+        <div className="font-link">
+          <div className="eggz">
             {address ? (
-        <>
-        {isMismatched}
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
-          <p>Your address: {address}</p>
-        </>
-      ) : (
-        <div className='connect'>
-        
-        
-
-        
+              <>
+                {isMismatched}
+                <button onClick={disconnectWallet}>Disconnect Wallet</button>
+                <p>Your address: {address}</p>
+              </>
+            ) : (
+              <div className="connect"></div>
+            )}
+          </div>
         </div>
-      )} 
+      </>
+    );
+  }
+
+  ///////***MEMBERS AREA *////////
+
+  if (hasClaimedNFT) {
+    return (
+      <div className="lg:h-screen lg:w-screen">
+        <iframe
+          src="https://player.thetavideoapi.com/video/video_gshi0kmd6qipqwsvzmwdt7nen9"
+          border="0"
+          width="100%"
+          height="100%"
+          allowfullscreen
+        />
       </div>
-      </div>
-             </>
-         )
-     }
+    );
+  }
 
-    
+  ////***NON - MEMBERS AREA *//////
 
-
-    
-    
-    
-     ///////***MEMBERS AREA *//////// 
-
-
-   if (hasClaimedNFT) {
-       return (
-        <>
-          <iframe src="https://player.thetavideoapi.com/video/video_gshi0kmd6qipqwsvzmwdt7nen9" 
-        border="0" 
-        width="100%" 
-        height="100%"
-        allowfullscreen/>
-        </>
-
-           
-       )
-   }
-
-   
-   
-
-   
-   
-   ////***NON - MEMBERS AREA *//////
-   
-   
-
-   return (
+  return (
     <>
-    <div className='eggz'>
+      <div className="eggz">
         <p>Purchase an EMOTE to unlock the Vid</p>
-
-
-    </div>
-    
+      </div>
     </>
-   );
-  };
+  );
+}
 
 export default Video;
