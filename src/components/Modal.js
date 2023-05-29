47b5a924dce5f5d854d9ef819d29c2a33854a3e0
ss251 <ss251@uw.edu>
2023-05-29 14:49:57 -0500

    create Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef } from "react";
+
+const Modal = ({ onClose, component: Component }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
+  return (
+    <div className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-opacity-50 bg-gray-500">
+      <div className="bg-black pl-4 pr-4 pb-4 shadow-md" ref={modalRef}>
+        <button
+          className="z-50 absolute top-0 right-0 m-2 text-white hover:text-gray-700"
+          onClick={onClose}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        <div className="mt-4">
+          <Component />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
