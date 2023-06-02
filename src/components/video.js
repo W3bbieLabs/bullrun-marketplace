e9a157b52512354380b568c6172ea7694432d613
ss251 <ss251@uw.edu>
2023-06-02 01:49:37 -0500

    potential fix for unresponsive play btn on mobile

--- src/components/video.js
+++ src/components/video.js
@@ -64,7 +64,7 @@ function Video({ url }) {
   }
 
   return (
-    <div className="lg:h-screen lg:w-screen z-50">
+    <div className="lg:h-screen lg:w-screen z-50 cursor-pointer">
       <iframe
         //src="https://player.thetavideoapi.com/video/video_gshi0kmd6qipqwsvzmwdt7nen9"
         src={url}
